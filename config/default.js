2f233c791a816cbd9de54a50964869790845b175
ax333l <51031206+ax333l@users.noreply.github.com>
2021-02-23 20:38:46 +0300

    Delete default.js

--- config/default.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  db: 'mongodb+srv://ax333l:SANJA1203@cluster0-uouja.mongodb.net/project?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
-};
-
