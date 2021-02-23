a52e9336a33c8340fa35ad7063a37e5edc6778a3
ax333l <51031206+ax333l@users.noreply.github.com>
2021-02-23 20:39:09 +0300

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/test1'
+  db: 'mongodb+srv://ax333l:SANJA1203@cluster0-uouja.mongodb.net/project?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true'
 };
 
